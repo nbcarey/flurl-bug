eeb70f43b322989e6b5d76cef4476be4f12b82c7
Nicholas Carey <nicholas.carey@nordstrom.com>
2016-04-18 18:54:06 -0700

    initial commit

---
+++ dotnet-461/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Flurl;
+using Flurl.Http;
+
+namespace dotnet_461
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Task<Stream> request = "https://www.google.com/"
+                                   .SetQueryParam( "q" , "foobar" )
+                                   .GetStreamAsync()
+                                   ;
+            request.Wait();
+            Stream stream = request.Result;
+            return;
+        }
+    }
+}
